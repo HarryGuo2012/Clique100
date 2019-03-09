11e3a69eb8abea5dc3e0aa4abc0adc34a6495393
Harry Guo <Harry.Guo@outlook.com>
2019-03-09 21:29:46 +0800

    fafafa

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,8 @@
-#include "../include/IO.h"
 #include "../include/Graph.h"
+#include "../include/IO.h"
 #include "../include/headers.h"
-#include "../include/"
+
+#include "../third_party/args.hxx"
 
 Graph *G;
 IO io;
@@ -9,10 +10,28 @@ IO io;
 std::string inputFile, outputFile;
 
 int main(int argc, char *argv[]) {
-	inputFile = argv[argc - 2];
-	outputFile = argv[argc - 1];
-	io.input(G, inputFile);
+    args::ArgumentParser parser("Clique100: A clique seeking algorithm\n",
+                                "Zhenyu Guo");
+    args::HelpFlag help(parser, "help", "Display this help menu",
+                        {'h', "help"});
+    args::Group required(parser, "", args::Group::Validators::All);
+    args::Positional<std::string> file(required, "file", "Data file");
 
-	
-	return 0;
+    try {
+        parser.ParseCLI(argc, argv);
+    } catch (args::Help) {
+        std::cout << parser;
+        return 0;
+    } catch (args::ParseError e) {
+        std::cerr << e.what() << std::endl;
+        std::cerr << parser;
+        return 1;
+    } catch (args::ValidationError e) {
+        std::cerr << e.what() << std::endl;
+        std::cerr << parser;
+        return 1;
+    }
+
+	io.input(G, args::get(file));
+    return 0;
 }
